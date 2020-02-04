fecafd9988a93a45a43b6e5a809a963bca38a885
wildintu <hello@wildintu.com>
2020-02-04 13:03:35 -0600

    arrow functions

--- script.js
+++ script.js
@@ -13,8 +13,21 @@ let name = 'first last';
 let singleName = name.split(' ');
 //console.log(singleName[0]);
 
-let getFirstName = name => console.log(`${singleName[0]}`);
+let getFirstName = function name() {
+    console.log(`${singleName[0]}`);
+};
 getFirstName();
 
 let getFirstNameConcise = name => console.log(`${singleName[0]}`);
 getFirstNameConcise();
+
+let dog = (x,y) => {
+    let benji = {
+        exponent: Math.pow(x,y),
+        product: x*y       
+    }
+    return benji;
+}
+
+woof = dog(2,5);
+console.log(`${woof.exponent} and ${woof.product}`)
