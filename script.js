779ab4718e00edbf68fd572e69b41b2e2741fc95
wildintu <hello@wildintu.com>
2020-02-04 11:14:45 -0600

    okay

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function favMovie(movie, name) {
-    console.log(`my name is ${name} and my favorite movie is ${movie}`);
-}
+// function favMovie(movie, name) {
+//     console.log(`my name is ${name} and my favorite movie is ${movie}`);
+// }
+
+let favMovie = (movie, name) => console.log(`my name is ${name} and my favorite movie is ${movie}`);
 
 favMovie();
 favMovie('The Room', 'world');
 favMovie('','');
+
+let name = 'first last';
+
+let singleName = name.split(' ');
+//console.log(singleName[0]);
+
+let getFirstName = name => console.log(`${singleName[0]}`);
+getFirstName();
+
+let getFirstNameConcise = name => console.log(`${singleName[0]}`);
+getFirstNameConcise();
